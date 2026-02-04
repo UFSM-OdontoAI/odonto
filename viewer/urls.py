c927dc525e2d5306a10035581d347bd24e45c8f4
Marcos Hideo Maruo <bootstrap666@github.com>
2026-02-04 18:19:36 -0300

    minimum functional prototype patch

--- viewer/urls.py
+++ viewer/urls.py
@@ -3,7 +3,10 @@ from . import views
 from .views_image import serve_image
 
 urlpatterns = [
- path('<str:split>/', views.image_list),
- path('<str:split>/<int:image_id>/', views.image_detail),
- path('image/<str:split>/<str:filename>/', serve_image),
+    path('<str:split>/', views.image_list, name='image_list'),
+    path('<str:split>/<int:image_id>/', views.image_detail, name='image_detail'),
+
+    # 🔧 ESTA LINHA É A FALTANTE
+    path('image/<str:split>/<str:filename>/', serve_image, name='serve_image'),
 ]
+
